ce3a5bf296c7cd404c570e22aa84fc8f05aba58b
olenapolko <olenapolko@gmail.com>
2024-05-28 15:39:36 +0300

    fixed comments

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -7,15 +7,19 @@ import {ProductsService} from '../../services/products.service';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-
 export class ProductsComponent implements OnInit {
   title = 'All products';
   products: ProductInterface[] = [];
-  isLoading = true;
+  isLoading = false;
 
   constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
+    this.subscribeToProducts();
+  }
+
+  subscribeToProducts(): void {
+    this.isLoading = true;
     this.productsService.getProducts().subscribe((products) => {
       this.products = products;
       this.isLoading = false;
